6465f5a9aed39b98feabd12b761976c6d8d26d36
Marco Pacifico <marco.pacifico@gmail.com>
2021-01-31 16:53:41 -0500

    added favicon with gatsby manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,24 @@ module.exports = {
     title: `Marco Pacifico`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Marco Pacifico",
+        short_name: "Marco Pacifico",
+        start_url: "/",
+        background_color: "#fcfcfa",
+        theme_color: "#fcfcfa",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/content/images/favicon.svg", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import styled from "styled-components"
 import theme from "../Theme"
 import {H1, H4} from '../Typography'
